2574ae68643be45114a6fe40a25c6c7972bf234d
Łukasz Papierz <pope88@gmail.com>
2017-05-22 23:22:04 +0200

    First approaches to event handling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,35 @@
+const items = [
+    'thingie',
+    'next thingie',
+    'more stuff',
+    'sth sth sth'
+];
+
 new Vue({
     el: '#app',
-        data: {
-            text: 'Hello world!'
+    data() {
+        return {
+            counter: 0,
+            message: 'Type your text here...',
+            text: 'Hello world!',
+            items: items
+        };
+    },
+    methods: {
+        increment() {
+            this.counter++;
+        }
     }
 });
+
+
+function listItems(){
+    let listHTML = '';
+    _(items).forEach((item) => {
+        listHTML += '<li>' + item + '</li>';
+    });
+    const contain = document.querySelector('#app');
+    //contain.innerHTML += '<ul>'+listHTML+'</ul>';
+}
+
+listItems();
